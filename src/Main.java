aebb6915b7d16de5321eac3a529f815a18e542f2
FiratYonak <firatyonak@gmail.com>
2020-04-07 12:16:37 +0300

    Game is implemented in main class.

--- src/Main.java
+++ src/Main.java
@@ -16,5 +16,32 @@ public class Main {
         ArrayList3.add(200);
         ArrayOrganizer org = new ArrayOrganizer();
         org.concatAndSort(ArrayList1,ArrayList2,ArrayList3);
+        Player Tom = new Player("Tom");
+        Player John = new Player("John");
+        Player James = new Player("James");
+        Player Henry = new Player("Henry");
+        Die die = new Die();
+        for ( int i = 0; i < 5; i++){
+            for ( int j = 0; j < 4; i++){
+                if( j == 0){
+                    Tom.addScore(die.rollDie());
+                }
+                else if( j == 1){
+                    John.addScore(die.rollDie());
+                }
+                else if( j == 2){
+                    James.addScore(die.rollDie());
+                }
+                else{
+                    Henry.addScore(die.rollDie());
+                }
+                
+            }
+        }
+        Tom.toString();
+        John.toString();
+        James.toString();
+        Henry.toString();
+
     }
 }
